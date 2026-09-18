48864622838beeca5cd9b88f2eea40f90b8d0edf
aditya-wuw <guddumandal605605@gmail.com>
2026-09-18 21:07:17 +0530

    Added: reroute if user logged in

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -47,12 +47,18 @@ export async function updateSession(request: NextRequest) {
   const { data } = await supabase.auth.getClaims();
   const user = data?.claims;
 
+  if (user && request.nextUrl.pathname === "/") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/home";
+    return NextResponse.redirect(url);
+  }
+
   if (
     request.nextUrl.pathname !== "/" &&
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/docs") 
+    !request.nextUrl.pathname.startsWith("/docs")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
