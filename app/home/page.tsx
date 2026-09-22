cf05f1ed5947f6950d30298bb5c3b658c3dcde58
aditya-wuw <guddumandal605605@gmail.com>
2026-09-22 17:48:10 +0530

    Added: Dashboard overview of contacts, published projects and journals

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,11 @@
 import { createClient } from "@/lib/supabase/server";
 import Dashboard from "@/components/Dashboard";
-import { Button } from "@/components/ui/button";
 import JournalForm from "@/components/Forms/Journal/JournalForm";
 import Toast from "@/components/Toast";
 import ProjectForm from "@/components/Forms/Projects/ProjectForm";
 import { LayoutDashboard, Plus } from "lucide-react";
 import RecentJournalsProjects from "@/components/RecentJournalsProjects";
+import { ContactsQuery, JournalType, ProjectsType } from "@/types/database";
 
 async function getCountDetails() {
   const supabase = await createClient();
@@ -36,14 +36,46 @@ async function getCountDetails() {
     return null;
   }
 }
-const ICON_SIZE = 16;
+
+const getRecentEvents = async () => {
+  const supabase = await createClient();
+  const { data: ContactsData, error: ContactsFetchError } = await supabase
+    .from("contact_queries")
+    .select("id,SenderName,origin,status,created_at")
+    .range(0, 3);
+
+  const { data: Projects, error: ProjectsFetchError } = await supabase
+    .from("personal_projects")
+    .select("id,title,Link,created_at")
+    .range(0, 3);
+
+  const { data: Journals, error: JournalsFetchError } = await supabase
+    .from("personal_blogs")
+    .select("id,title,published,updated")
+    .range(0, 3);
+
+  const error = ContactsFetchError || ProjectsFetchError || JournalsFetchError;
+
+  if (error) {
+    console.error(error.message);
+  }
+
+  // console.log(Projects, Journals);
+  return {
+    contacts: ContactsData ?? [],
+    projects: Projects ?? [],
+    journals: Journals ?? [],
+  };
+};
+
 export default async function ProtectedPage() {
   const Counts = await getCountDetails();
+  const { contacts, journals, projects } = await getRecentEvents();
   return (
     <div className="relative flex-1 w-full flex flex-col pt-4 px-4 pb-5">
       <Toast />
       <h1 className="mb-4 flex items-center gap-2">
-        <LayoutDashboard size={ICON_SIZE} />
+        <LayoutDashboard size={16} />
         Dashboard
       </h1>
       <Dashboard
@@ -52,7 +84,7 @@ export default async function ProtectedPage() {
         ContactPending={Counts?.TotalContacts ?? 0}
       />
       <h1 className="mt-10 flex items-center gap-2">
-        <Plus size={ICON_SIZE} />
+        <Plus size={16} />
         Create Somthing
       </h1>
       <div className="w-full flex gap-3 mt-5">
@@ -60,9 +92,9 @@ export default async function ProtectedPage() {
         <ProjectForm />
       </div>
       <RecentJournalsProjects
-        RecentJournals={[]}
-        RecentProjects={[]}
-        RecentContacts={[]}
+        RecentJournals={journals}
+        RecentProjects={projects}
+        RecentContacts={contacts}
       />
     </div>
   );
--- components/RecentJournalsProjects.tsx
+++ components/RecentJournalsProjects.tsx
@@ -2,12 +2,36 @@
 
 import { ContactsQuery, JournalType, ProjectsType } from "@/types/database";
 import { Card } from "./ui/card";
-import { ClockFading, ContactIcon } from "lucide-react";
+import {
+  ClockFading,
+  ContactIcon,
+  EarthIcon,
+  ExternalLink,
+  User2,
+} from "lucide-react";
+import { getFormatedDate } from "@/lib/utils/getFormatedDates";
+import Link from "next/link";
 
 interface props {
-  RecentJournals: JournalType[];
-  RecentProjects: ProjectsType[];
-  RecentContacts: ContactsQuery[];
+  RecentJournals: {
+    id: string;
+    title: string;
+    published: string | null;
+    updated: string | null;
+  }[];
+  RecentProjects: {
+    id: string;
+    title: string | null;
+    Link: string | null;
+    created_at: string;
+  }[];
+  RecentContacts: {
+    id: string;
+    SenderName: string;
+    origin: string;
+    status: string | null;
+    created_at: string | null;
+  }[];
 }
 
 export default function RecentJournalsProjects({
@@ -17,50 +41,139 @@ export default function RecentJournalsProjects({
 }: props) {
   return (
     <div className="w-full  mt-8">
-      <div className="flex flex-col gap-4">
+      <Link href={"/home/contacts"} className="flex flex-col gap-4">
         <h1 className="flex-items gap-2">
           <ContactIcon size={16} />
           Recent contacts
         </h1>
         <Card className="p-4 w-full">
           {!RecentContacts || RecentContacts.length === 0 ? (
-            <h1 className="opacity-80 text-sm flex-center p-10">
+            <h1 className="text-sm flex-center p-10 opacity-50">
               {" "}
               no new contact queries to show
             </h1>
           ) : (
-            <div></div>
+            RecentContacts.sort((a, b) => {
+              const a_created_at = new Date(a.created_at ?? "").getTime();
+              const b_created_at = new Date(b.created_at ?? "").getTime();
+              const sorted = b_created_at - a_created_at;
+              return sorted;
+            }).map((i) => (
+              <div
+                className="xl:p-3 p-2 flex max-xl:flex-col xl:justify-between"
+                key={i.id}
+              >
+                <div className="grid grid-cols-3 grid-rows-1 gap-5 xl:w-[40%]">
+                  <h1 className="flex-items gap-1.5 w-25">
+                    <User2 size={16} />
+                    {i.SenderName}
+                  </h1>
+                  <h1 className="flex-items gap-1.5 w-15">
+                    <EarthIcon size={16} />
+                    {i.origin}
+                  </h1>
+                  <h1 className="flex-center gap-1.5 bg-gray-900/40 p-2 rounded-2xl">
+                    {i.status}
+                  </h1>
+                </div>
+                <div className="flex-items gap-3 opacity-80 text-sm">
+                  <h1>contacted</h1>
+                  <h1 className="">
+                    {getFormatedDate(new Date(i.created_at ?? "")) ?? ""}
+                  </h1>
+                </div>
+              </div>
+            ))
           )}
+          <h1 className="text-blue-500/60 hover:text-blue-500 text-sm px-2">
+            go to contacts
+          </h1>
         </Card>
-      </div>
+      </Link>
 
       <div className="flex flex-col gap-4 mt-5">
         <h1 className="flex-items gap-2">
           <ClockFading size={16} />
           Recent posts
         </h1>
-        <Card className="p-4 mt-2 w-full">
-          <h1 className="opacity-80 text-md">Recent Journals</h1>
-          {!RecentJournals || RecentJournals.length === 0 ? (
-            <h1 className="opacity-80 text-sm flex-center p-10">
-              {" "}
-              no new journals or drafts to show
+        <Link href={"/home/journals"}>
+          <Card className="p-4 mt-2 w-full">
+            <h1 className="opacity-80 text-md">Recent Journals</h1>
+            {!RecentJournals || RecentJournals.length === 0 ? (
+              <h1 className="text-sm flex-center p-10 opacity-50">
+                {" "}
+                no new journals to show
+              </h1>
+            ) : (
+              <div>
+                <div>
+                  {RecentJournals.map((i) => (
+                    <div
+                      key={i.id}
+                      className="flex max-xl:flex-col xl:justify-between mt-2"
+                    >
+                      <div className="grid grid-cols-2 gap-10">
+                        <h1 className="w-50  overflow-hidden">{i.title}</h1>
+                        <div className="relative z-100 flex-items text-nowrap gap-2 underline text-blue-500 overflow-hidden text-ellipsis">
+                          <ExternalLink size={16} />
+                          {i.id}
+                        </div>
+                      </div>
+                      <div>
+                        <h1 className="text-sm opacity-50">
+                          {getFormatedDate(
+                            new Date(
+                              i.updated ? i.updated : (i.published ?? ""),
+                            ),
+                          ) ?? ""}
+                        </h1>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            <h1 className="text-blue-500/60 hover:text-blue-500 text-sm px-2">
+              go to journals
             </h1>
-          ) : (
-            <div></div>
-          )}
-        </Card>
-        <Card className="p-4 w-full">
-          <h1 className="opacity-80 text-md">Recent projects</h1>
-          {!RecentProjects || RecentProjects.length === 0 ? (
-            <h1 className="opacity-80 text-sm flex-center p-10">
-              {" "}
-              no new projects or drafts to show
+          </Card>
+        </Link>
+        <Link href={"/home/projects"}>
+          <Card className="p-4 w-full">
+            <h1 className="opacity-80 text-md">Recent projects</h1>
+            {!RecentProjects || RecentProjects.length === 0 ? (
+              <h1 className="text-sm flex-center p-10 opacity-50">
+                {" "}
+                no new projects to show
+              </h1>
+            ) : (
+              <div className="p-3">
+                {RecentProjects.map((i) => (
+                  <div
+                    key={i.id}
+                    className="flex max-xl:flex-col xl:justify-between mt-2"
+                  >
+                    <div className="grid grid-cols-2 gap-10">
+                      <h1 className="w-50  overflow-hidden">{i.title}</h1>
+                      <div className="relative z-100 flex-items text-nowrap gap-2 underline text-blue-500 overflow-hidden text-ellipsis">
+                        <ExternalLink size={16} />
+                        {i.Link}
+                      </div>
+                    </div>
+                    <div>
+                      <h1 className="text-sm opacity-50">
+                        {getFormatedDate(new Date(i.created_at)) ?? ""}
+                      </h1>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+            <h1 className="text-blue-500/60 hover:text-blue-500 text-sm px-2">
+              go to projects
             </h1>
-          ) : (
-            <div></div>
-          )}
-        </Card>{" "}
+          </Card>
+        </Link>
       </div>
     </div>
   );
